f17a9a02f020c46cebc585bd89b5cb4bcf7be39d
Dashkako <Dasha05971@gmail.com>
2023-09-24 18:42:59 +0300

    1

--- DZ3.py
+++ DZ3.py
@@ -51,9 +51,9 @@ for i in stroc:
 print(f"В строке {stroc} = {count} {simvol}")
 
 #Задание 5
-a = str(input("Введите предложение: "))
+a = str(input("Введите предложение: ")).lower()
 alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" * 2
-num = 3
+num = int(input("Введите количество строчек сдвига: "))
 
 def shifr(first_str:str, step:int)->str:
     b = ""
@@ -82,6 +82,19 @@ print(result)
 print(deshifr(result, num))
 
 #Задание 6
+count = int(input("Введите количество чисел в списке: "))
+nums = [int(input("Введите число: ")) for a in range(count)]
+def lists(count:int, nums:list)->tuple:
+    otr = list()
+    not_otr = list()
+    for i in nums:
+        if i < 0:
+            otr.append(i)
+        else:
+            not_otr.append(i)
+    return sorted(otr, reverse=True), sorted(not_otr)
+
+print(lists(3, nums))
 
 #Задание 7
 stroc = str(input("Введите строку: "))
@@ -99,6 +112,56 @@ else:
     else:
         print("Список не является палиндромом")
 
+#Задание 8
+
+def number():
+    start = 1
+    end = 100
+    avg = (start + end) // 2
+    step = 0
+    while True:
+         a = str(input(f"Твое число равно, меньше или больше {avg}: ")).lower()
+         step += 1
+         if a == "равно":
+             print(f"Число {avg}, кол-во попыток {step}")
+             break
+         elif a == "больше":
+             start = avg + 1
+             avg = (start + end) // 2
+         elif a == "меньше":
+             end = avg - 1
+             avg = (start + end) // 2
+         else:
+             print("Неверная команда")
+             step -= 1
+
+#Задание 9
+
+def convert_to10(num, sistem):
+    a = 0
+    alfabeth = "0123456789ABCDEF"
+    alf = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    for i in range(len(num)):
+        n = alf[alfabeth.index(num[i])]
+        a += n * (sistem**(len(num)-i-1))
+    return a
+
+print(convert_to10("45AB", 16))
+
+def convert_to2(num, sistem):
+    a = ""
+    alfabeth = "0123456789ABCDEF"
+    alf = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    while True:
+        a += str(num % 2)
+        num = num // 2
+        if num == 0:
+            break
+        # n = alf[alfabeth.index(num[i])]
+        # a += n * (sistem**(len(num)-i-1))
+    return "".join(reversed(a))
+
+print(convert_to2(convert_to10("45AB", 16), 10))
 
 #Задание 10
 import datetime
